343ab66dfb1df855e7358fd02237c1230ef2282c
Archakov Dennis <amonbower@gmail.com>
2020-11-30 01:40:31 +0300

    fix

--- practice/build/service-worker.js
+++ practice/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0b09e25194e14248809a9aea6c9fb37.js"
+  "/precache-manifest.5bfdeaaf6876593b89bcaaf1213c22f3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
